7d5a3c800fdfe114f2db3f6e3e31de0ac2aaed7e
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2023-01-11 15:42:13 +0100

    Add testing all devnet pools

--- src/lib.rs
+++ src/lib.rs
@@ -185,8 +185,8 @@ impl Amm for JupiterInvariant {
         let tickmap = Self::deserialize::<Tickmap>(tickmap_account_data);
 
         let ticks = accounts_map.into_iter()
-        .filter(|(key, _)| !self.market_key.eq(key) && !self.pool.tickmap.eq(key))
-        .collect::<HashMap<&Pubkey, &Vec<u8>>>().into_iter().map(|(key, data)| {
+            .filter(|(key, _)| !self.market_key.eq(key) && !self.pool.tickmap.eq(key))
+            .collect::<HashMap<&Pubkey, &Vec<u8>>>().into_iter().map(|(key, data)| {
             let tick = Self::deserialize::<Tick>(data);
             (*key, tick)
         }).collect::<HashMap<Pubkey, Tick>>();
@@ -218,12 +218,15 @@ impl Amm for JupiterInvariant {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
     use anchor_lang::prelude::Pubkey;
     use anchor_lang::{AnchorDeserialize, AnchorSerialize};
     use decimal::*;
     use invariant_types::decimals::FixedPoint;
     use invariant_types::structs::{FeeTier, Pool};
+    use solana_client::client_error::reqwest::blocking::get;
     use solana_client::rpc_client::RpcClient;
     use solana_sdk::account::Account;
     use solana_sdk::pubkey;
@@ -260,6 +263,142 @@ mod tests {
 
         jupiter_invariant.ticks.iter().for_each(|(_, tick)| {
             println!("{:?}", tick);
+        });
+    }
+
+    #[test]
+    fn test_fetch_all_poo() {
+        let rpc = RpcClient::new("https://divine-multi-research.solana-devnet.discover.quiknode.pro/51c4bf9b6daf33ca9abf53799a89a17088f68a77");
+        let pool_addresses = vec![
+            "8krGUkrubzsymUFmy5ncJHiE5RwyDEWazvwfahii7UW1",
+            "51DNyWAJ7pa67DzkbHfipbZYW5hUSSRYsec5iHndtZ2W",
+            "BZSp8EvV8XTfNrsyBXrF2xGB3L7SbHSwhmJ7RD74zmAH",
+            "3a3EdoS5MvDcw9K8GdAb2FD7A9cUK6TuAqboJX23DQNM",
+            "71QzPrqM26vMDJdtanPqyNyQ7MzqVw3FqzUWTpdNxta2",
+            "4NkYxZtSpUzQc2oxktQoRYUSYC4awrtspuAGLq7Sx8A1",
+            "Fb8rVemMjXE9r7FNK7DTmUdDhQDa3jFufkqrH2bsDYWX",
+            "FKh586sYqro7RsTYsr7Wwc6b95bdkWCLEDKRc8fmBruh",
+            "9z35ptatPpnzzfzCXB5k7aJ6jxg4Qyno5xXVMLC7oHAW",
+            "Hb3evEtCB4ykLYYKFGkGgVuaCyrWFbXwe3jtkMSkq6Ve",
+            "7cRPNt4EMSwvmwSrm379JhyG7s49CuCbCfnARzGykkzx",
+            "AWnsFuKeTT3QDDinjCKsc5rM879iFQQz5rLq4JVNMhXF",
+            "7Pzoefg48HurAC8ewRfgJgJmvGSasMYi5zHgQ67cxGTS",
+            "8qhdhHUPxCqXDqhYiXDjjDnLAHctt7pEettHU6bTSXFB",
+            "CoD1yTtv1rftMhmQJSRwqci547iKERgjPhqcxNfXSwZF",
+            "9chT2PBZvM3TLfPQeHWyWREMUY8eLVp2uK5NnkGh3upt",
+            "ymuFCM1UFr6AMmtHTHKbjm6GzmAaRVAFsGVwqWEhArj",
+            "BWKw74TeWArB8TiEczNvihcsixadptis9sNwSTcLbwxo",
+            "5yjxDcJzH1yaKYA6AH7GEgxtC6rdqy2kSCifjMLcmh8b",
+            "CNDKBRwcagYkjfTPYB91DZsQwCQJfxrcywAdVQKnDJb1",
+            "EKYai7Zh1qPtRtkkYJcCvdEPo2fXj3bfuTGoNUSJ4Z1m",
+            "2ShQGNv3YNqABBSEVGwSk9s1UCaDMGGehdbnjiJ1HmYn",
+            "D7rbZbEXZDMRepqDbvPorN26e1E6Qs5UoYnzLU9KqV3q",
+            "33AiuucYaKGbCoSNBAuzdKCHZ5Svv7GS9DiZcVWbmygd",
+            "ENDCh3U3BPg1Jw3pRKu9Vb4dUeRSFqqR5DAXhZZYpEgR",
+            "7p9iJfB1ToLYv55JontFhAt1K5rA57oa9AWyrZRaRX4A",
+            "CrMFwYDc3j4d9f9dogyDTx97raJJpAupntzVXm2bL8hH",
+            "CHQ8Ti9L3z9aLfML1ULf78ZaGFwaTMA6GFcteBvMgmAG",
+            "GGAtxXgYGP5YzxbhhnuYCwZJV24idnSp31XK81Lqu6vi",
+            "HbJhdrHeagEKTKLiajwXSvgETRs652U3vcMHpQS5y8qr",
+            "ApkrPbiWzEmkRj92g8tpzMNNXGkTogRNMQfESaUQAykv",
+            "41PkE4UvqTeqNSjJ5S1oTGpt3bBHhcwaFBCJ6wL175NY",
+            "ByREwqGG5AJeKgRSKavwbkyJsNfAx9sHEwzeh3HCwaWD",
+            "2Lh9wSQnHkr3g3sgj5QJYqeRCQrYChX7ANwjjvs5rDGo",
+            "7WxatmehVQjfXYMXoyFXnrRMCpDnMKcShFhNDXUioDpb",
+            "27j5twe2g8Vqr6bGyEjX4dSPKA3s1hu4N1nm9u3gRsmc",
+            "79RBFWfLKbJMNHzF6sqsAb7z9iXY1kyNFpuW6HJQQW4f",
+            "BvPLZDd4cHfNeUy3F9tv8PQkJSWtwe5EyFpyTm81tQ9g",
+            "GAd62dWPW36cxUPndsPwB52x1xjeRcJMuZ2SNdZTMzDR",
+            "F8fj2myqZcw5CZy8yaNZDJXS2rzB7FL79maFBJM16WPu",
+            "6W6iaKmdBNqyFrXmJqB5yrebr3KowYDs4F9Nide4ARYL",
+            "82g9tPxdE3UgLiSoA4KkV6S7hXKdN4RDjcbBiqTwWSWw",
+            "As83NX9BsHUx5RBuRWGWzByzXsSEoThVzcTpHrNkvUFt",
+            "YkDo4ooVf3Sw79y84w3HCy7aGFd6UMfkGdgDMiHdprx",
+            "2Ag3cXASMvi9aNTc3iPT2R31zVxQXeawhsx4Ukf2xTeH",
+            "7Qm14zJHs8RVFjyWvauw945Usygjv3v3kP5nGrq8Hbij",
+            "ChQYkyvvuAb25t6bWQzVpNytA25D2szmTMJusQLwUBpi",
+            "DkFWVeEchx65Q9vymQjDjdfY472vV4JvkXompH5s4u6u",
+            "74nxLDML7sZeFjeqLteu89kGxC18ZRdsSHsMP9q5BALP",
+            "8PhY9FH6ZQT74nnBuxxoVs4nYk9NUWwguFfWrZHGiAwK",
+            "BkwfZY9mrZyZRRDSFE6joFdDrvrXhGq2gpBXXecyjKAn",
+            "5KdKapBaBy8BSCNst8qG4nxauE92KaBZDxJNXAGZpCGA",
+            "HyXFTA4sq7NM4Bs49zPu25T1fzE5KXabafoiC4qAWjxR",
+            "CmkfvxXTLihwp7eBm9HFtfGTvBHfK8cKPW99LEq1uR7c",
+            "91efQM48sjFp5YPLRPVVFwxoER2YiYzy9BfhPeAk8oNS",
+            "GYXWHy8iakBiffXh6NinMnxkuJkmNrMPry6zCPQdeaJ2",
+            "7JivSGvY3ohqtGjmPZq59DqMJQ6FT73SeFPsH1nzypuP",
+            "6fJ7t3uvmosPqXY51Qcwdc126miSnTF2bt7xgG5fyPgX",
+            "6zPRRm2tjgUYPrnSboaxr2RvovB9y5vT9f4kYyzdbbri",
+            "H9AcSjr1N9PX8pXGef98MS8sFL5p7bqLtPb71xeXo5Nn",
+            "569xM2bZSTijhdB8u15G6e442PVq3PhcJUPFUZM9njvi",
+            "BbuK8MkcZSJxx7VtRXB8Up8CqeURv3FRHzdzavPyKr1H",
+            "6SXW9UNVdiUWJhw4RvfmovbkTEBeb8tvBzPHc8EeVWi",
+            "6m8H4nKpm5fEQqKBaniRUYQfeaH7pjMkPPFiMnGXZEd8",
+            "D8W6t585rTrYfhtQQTxozyG6w2oCLpBwLCy7rMsS1rNF",
+            "7iZvgThSvJv95mkWnPPE6LENVcXTKfCVV2A4k3pZv9Te",
+            "C7QuXVtEF3dm1ocF5nsigfPqRXkdH2oNr5683gs8SacC",
+            "7SaJ5xEHTtWrFoJcNrNkJKb4pHJSDoMtzvcyHXJuhZRf",
+            "A7AwVVu1y1bzz86LQrLbVtgC43k7LES6Lesc8ao7MGFP",
+            "7rk54mPCRAg3jKCioCVK5wganTsGnn4ZYpCT9uTWzHi7",
+            "AAaXsTU7hWFUY6UPTm2MfK8esLrh2P17M2e9kHSPwRMZ",
+            "Hb2freG61xfMgmH6CUJDiyjG5zbbfcp9a6TQ2SJvs2Ur",
+            "A6VbaGsFNZ3GBiin7XMRS2cwvbVpys8AKgnRmKSNvBwX",
+            "6cNPxiBvD6xzA6UbKuNjuyRfbGpprdG3nRPdTYyZGwST",
+            "HUmPDU5amyLXjpZJGwMf6YAa6W9LW2srJjcrSM3FXgUA",
+            "5Wc3GkVnPmfRMgumstAEtT1yWYR7nBeyjYEQonz91bpb",
+            "NvbSrT458e6LCeLq257rH73dZ7btcq1TPkQVsyL8ouT",
+            "AG4harXetHAJLmKfQRTvW8uBBhjGDqbSvMQGTAhik4eE",
+            "6nqWUFzjoJzuZ9FWF6iBjFwUpGX6mZrF25WaWsbwqekM",
+            "9gGUAz4psCE2t3NFr8p64hgvXAcQGADtLmrNDTKMJPpC",
+            "45t3M59UDEkeADxyGq1tJxwoUPQ5kUxeuLsusYBFkBah",
+            "FmV9BnyyAGqVWudkV2S4hq7TGkUfoa8ku6UhKK1UN9zn",
+            "8cQaC9iDd9pGQYcZfK61zgMitBvsW7218WzG55JhonsZ",
+            "CC72FniNhUM41xSoA5h1idGxstrKXDqqhnYzfyBTSZDG",
+            "Cy1zKgjXT5PREXaw72tu8sWjxwiTpmnwn1kzhMKaCrtb",
+            "A4maCv1LM7rjpUzzM7JP4wg61VchwCovKiRqo3fbXSgF",
+            "4ervEtFsXtxBJCshqaXbHBbv7xWz4XsSp6ptDPEJRq53",
+            "93b5xNohGF24cbbGn7pw4QUKGLcrXdBVGw8gRsxTNcUx",
+            "84GSk7hjwaUwX5wxmvnHiTtSnffPYP7dnmoP6ZJ5TnSR",
+            "hLZNVV8EbeLCjkXCxfBePjAcuPSwCryWtiStoV7E4cU",
+            "HZwUnXYy27TUWQucuuSM8dYJxxCgUMgfDf83vZNz7CKe",
+            "CqVgxaQprGDZkBnAFNxKnwgaB5929CT1nSFxYjgUJz4H",
+            "Ee7EsMJ6bYN99FKVTc9SEpX9HucyHg4AGuQpRuP9Ac4c",
+            "Di5eqFCEonm8ABWfZph4fermaFbtrU5Jj2mrYdkKM1pT",
+            "bfSBHwyL6PNjpeQEyrjsnbo7TzGRttbymvN74Y9czN3",
+            "AgUfgteJ5mGWeuKZeUTedwvvu8jLaWGiRE2matEMoHEG",
+            "xQob6CJEuPD23kbMENHWGWT7ho4Anwx4dpH7pBbX6MY",
+            "6Cr9rtgY4c7H6DZzaCTgVpeFK8TpdcuhZXeZ2wqPhCD3",
+            "CssW9uGzH22u9CUkft1xgqHgKCGCTrCL6Ma93wLY94dq",
+            "GBEo8NoJAg5woWXXWXKT5xaWVUmxsqYgLkvyLkFDEgzY",
+            "4WeKjRD14BGktxENsyE6hf8SBmmxY4F56V7TEMPrQ6nR",
+            "7w3qaYPMAnbHnyWKqDzHVZcTLfJaDXhB2JdDUkZXuMho",
+            "EUdiwR5oc6YfH3Y2AzQt5fUJAPB6F1kHyiqW8ZM5m5mg",
+            "BWuHaUGmRKqGew1hffbCnfzkNNn4XLToyYp5zw8LiqSo",
+            "EGLDBGNDaC1pr3iznmGDUSVLmFE8DGR1LruUHTwCFhhy",
+        ];
+        println!("len = {:?}", pool_addresses.len());
+        let pubkeys: Vec<Pubkey> = pool_addresses.iter().map(|p| { return Pubkey::from_str(*p).unwrap() }).collect::<Vec<Pubkey>>();
+        // let a = &pubkeys[0..99];
+
+        rpc.get_multiple_accounts(&pubkeys[0..5]).unwrap().iter().enumerate().for_each(|(index, market_account)| {
+            let key: Pubkey = pubkeys.get(index).unwrap().to_owned();
+            let account = market_account.to_owned().unwrap();
+            let mut jupiter_invariant =
+                JupiterInvariant::new_from_keyed_account(&KeyedAccount {
+                    key,
+                    account,
+                    params: None,
+                }).unwrap();
+            let accounts_to_update = jupiter_invariant.get_accounts_to_update();
+            let accounts_map = JupiterInvariant::fetch_accounts_map(&rpc, accounts_to_update);
+            jupiter_invariant.update(&accounts_map).unwrap();
+            let accounts_to_update = jupiter_invariant.get_accounts_to_update();
+            let accounts_map = JupiterInvariant::fetch_accounts_map(&rpc, accounts_to_update);
+            jupiter_invariant.update(&accounts_map).unwrap();
+
+            jupiter_invariant.ticks.iter().for_each(|(_, tick)| {
+                println!("{:?}", tick);
+            });
         });
     }
 }
