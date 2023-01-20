ff4afd857459f17f32bee064ee50d16b686fc83f
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2023-01-20 04:37:54 +0100

    Add README.md

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Jupiter Invariant Integration
 
 Implements of the `Amm` trait defined [here](https://github.com/jup-ag/rust-amm-implementation).
+
+The following command will run the test:
+```shell
+cargo test test_jupiter_invariant -- --nocapture
+```
+The result is quote for selling 1 USDC on USDC/USDT Invariant market. Example response:
+```
+insufficient liquidity: false
+input amount: 1.000000 USDC
+output amount: 1.000056 USDT
+fee amount: 0.000010 USDC
+```
+
--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,6 @@ use solana_client::rpc_client::RpcClient;
 use solana_sdk::pubkey;
 
 pub const PROGRAM_ID: Pubkey = pubkey!("HyaB3W9q6XdA5xwpU4XnSZV94htfmbmqJXZcEbRaJutt");
-// pub const PROGRAM_ID: Pubkey = pubkey!("9aiirQKPZ2peE9QrXYmsbTtR7wSDJi2HkQdHuaMpTpei"); // devnet
 
 #[derive(Clone, Default)]
 pub struct JupiterInvariant {
@@ -233,7 +232,6 @@ impl JupiterInvariant {
             }
         }
 
-        // translate index in tickmap to price tick index
         found.iter().map(|i: &i32| {
             (i - TICK_LIMIT) * tick_spacing
         }).collect()
@@ -515,14 +513,18 @@ mod tests {
         jupiter_invariant.update(&accounts_map).unwrap();
 
         let quote = QuoteParams {
-            in_amount: 15_000 * 10u64.pow(6),
-            input_mint: USDT,
-            output_mint: USDC,
+            in_amount: 1 * 10u64.pow(6),
+            input_mint: USDC,
+            output_mint: USDT,
         };
         let result = jupiter_invariant.quote(&quote).unwrap();
-        println!("{:?}", result);
 
-        let swap_leg_and_account_metas = jupiter_invariant.get_swap_leg_and_account_metas(&SwapParams {
+        println!("insufficient liquidity: {:?}", result.not_enough_liquidity);
+        println!("input amount: {:.6} USDC", result.in_amount as f64 / 10u64.pow(6) as f64);
+        println!("output amount: {:.6} USDT", result.out_amount as f64 / 10u64.pow(6) as f64);
+        println!("fee amount: {:.6} USDC", result.fee_amount as f64 / 10u64.pow(6) as f64);
+
+        let _swap_leg_and_account_metas = jupiter_invariant.get_swap_leg_and_account_metas(&SwapParams {
             source_mint: quote.input_mint,
             destination_mint: quote.output_mint,
             user_destination_token_account: Pubkey::new_unique(),
@@ -532,122 +534,66 @@ mod tests {
             quote_mint_to_referrer: None,
             in_amount: quote.in_amount,
         }).unwrap();
-        println!("{:?}", swap_leg_and_account_metas.account_metas);
     }
 
     #[test]
     fn test_fetch_all_pool() {
-        let rpc = RpcClient::new("https://divine-multi-research.solana-devnet.discover.quiknode.pro/51c4bf9b6daf33ca9abf53799a89a17088f68a77");
+        let rpc = RpcClient::new("https://tame-ancient-mountain.solana-mainnet.quiknode.pro/6a9a95bf7bbb108aea620e7ee4c1fd5e1b67cc62");
         let pool_addresses = vec![
-            "8krGUkrubzsymUFmy5ncJHiE5RwyDEWazvwfahii7UW1",
-            "51DNyWAJ7pa67DzkbHfipbZYW5hUSSRYsec5iHndtZ2W",
-            "BZSp8EvV8XTfNrsyBXrF2xGB3L7SbHSwhmJ7RD74zmAH",
-            "3a3EdoS5MvDcw9K8GdAb2FD7A9cUK6TuAqboJX23DQNM",
-            "71QzPrqM26vMDJdtanPqyNyQ7MzqVw3FqzUWTpdNxta2",
-            "4NkYxZtSpUzQc2oxktQoRYUSYC4awrtspuAGLq7Sx8A1",
-            "Fb8rVemMjXE9r7FNK7DTmUdDhQDa3jFufkqrH2bsDYWX",
-            "FKh586sYqro7RsTYsr7Wwc6b95bdkWCLEDKRc8fmBruh",
-            "9z35ptatPpnzzfzCXB5k7aJ6jxg4Qyno5xXVMLC7oHAW",
-            "Hb3evEtCB4ykLYYKFGkGgVuaCyrWFbXwe3jtkMSkq6Ve",
-            "7cRPNt4EMSwvmwSrm379JhyG7s49CuCbCfnARzGykkzx",
-            "AWnsFuKeTT3QDDinjCKsc5rM879iFQQz5rLq4JVNMhXF",
-            "7Pzoefg48HurAC8ewRfgJgJmvGSasMYi5zHgQ67cxGTS",
-            "8qhdhHUPxCqXDqhYiXDjjDnLAHctt7pEettHU6bTSXFB",
-            "CoD1yTtv1rftMhmQJSRwqci547iKERgjPhqcxNfXSwZF",
-            "9chT2PBZvM3TLfPQeHWyWREMUY8eLVp2uK5NnkGh3upt",
-            "ymuFCM1UFr6AMmtHTHKbjm6GzmAaRVAFsGVwqWEhArj",
-            "BWKw74TeWArB8TiEczNvihcsixadptis9sNwSTcLbwxo",
-            "5yjxDcJzH1yaKYA6AH7GEgxtC6rdqy2kSCifjMLcmh8b",
-            "CNDKBRwcagYkjfTPYB91DZsQwCQJfxrcywAdVQKnDJb1",
-            "EKYai7Zh1qPtRtkkYJcCvdEPo2fXj3bfuTGoNUSJ4Z1m",
-            "2ShQGNv3YNqABBSEVGwSk9s1UCaDMGGehdbnjiJ1HmYn",
-            "D7rbZbEXZDMRepqDbvPorN26e1E6Qs5UoYnzLU9KqV3q",
-            "33AiuucYaKGbCoSNBAuzdKCHZ5Svv7GS9DiZcVWbmygd",
-            "ENDCh3U3BPg1Jw3pRKu9Vb4dUeRSFqqR5DAXhZZYpEgR",
-            "7p9iJfB1ToLYv55JontFhAt1K5rA57oa9AWyrZRaRX4A",
-            "CrMFwYDc3j4d9f9dogyDTx97raJJpAupntzVXm2bL8hH",
-            "CHQ8Ti9L3z9aLfML1ULf78ZaGFwaTMA6GFcteBvMgmAG",
-            "GGAtxXgYGP5YzxbhhnuYCwZJV24idnSp31XK81Lqu6vi",
-            "HbJhdrHeagEKTKLiajwXSvgETRs652U3vcMHpQS5y8qr",
-            "ApkrPbiWzEmkRj92g8tpzMNNXGkTogRNMQfESaUQAykv",
-            "41PkE4UvqTeqNSjJ5S1oTGpt3bBHhcwaFBCJ6wL175NY",
-            "ByREwqGG5AJeKgRSKavwbkyJsNfAx9sHEwzeh3HCwaWD",
-            "2Lh9wSQnHkr3g3sgj5QJYqeRCQrYChX7ANwjjvs5rDGo",
-            "7WxatmehVQjfXYMXoyFXnrRMCpDnMKcShFhNDXUioDpb",
-            "27j5twe2g8Vqr6bGyEjX4dSPKA3s1hu4N1nm9u3gRsmc",
-            "79RBFWfLKbJMNHzF6sqsAb7z9iXY1kyNFpuW6HJQQW4f",
-            "BvPLZDd4cHfNeUy3F9tv8PQkJSWtwe5EyFpyTm81tQ9g",
-            "GAd62dWPW36cxUPndsPwB52x1xjeRcJMuZ2SNdZTMzDR",
-            "F8fj2myqZcw5CZy8yaNZDJXS2rzB7FL79maFBJM16WPu",
-            "6W6iaKmdBNqyFrXmJqB5yrebr3KowYDs4F9Nide4ARYL",
-            "82g9tPxdE3UgLiSoA4KkV6S7hXKdN4RDjcbBiqTwWSWw",
-            "As83NX9BsHUx5RBuRWGWzByzXsSEoThVzcTpHrNkvUFt",
-            "YkDo4ooVf3Sw79y84w3HCy7aGFd6UMfkGdgDMiHdprx",
-            "2Ag3cXASMvi9aNTc3iPT2R31zVxQXeawhsx4Ukf2xTeH",
-            "7Qm14zJHs8RVFjyWvauw945Usygjv3v3kP5nGrq8Hbij",
-            "ChQYkyvvuAb25t6bWQzVpNytA25D2szmTMJusQLwUBpi",
-            "DkFWVeEchx65Q9vymQjDjdfY472vV4JvkXompH5s4u6u",
-            "74nxLDML7sZeFjeqLteu89kGxC18ZRdsSHsMP9q5BALP",
-            "8PhY9FH6ZQT74nnBuxxoVs4nYk9NUWwguFfWrZHGiAwK",
-            "BkwfZY9mrZyZRRDSFE6joFdDrvrXhGq2gpBXXecyjKAn",
-            "5KdKapBaBy8BSCNst8qG4nxauE92KaBZDxJNXAGZpCGA",
-            "HyXFTA4sq7NM4Bs49zPu25T1fzE5KXabafoiC4qAWjxR",
-            "CmkfvxXTLihwp7eBm9HFtfGTvBHfK8cKPW99LEq1uR7c",
-            "91efQM48sjFp5YPLRPVVFwxoER2YiYzy9BfhPeAk8oNS",
-            "GYXWHy8iakBiffXh6NinMnxkuJkmNrMPry6zCPQdeaJ2",
-            "7JivSGvY3ohqtGjmPZq59DqMJQ6FT73SeFPsH1nzypuP",
-            "6fJ7t3uvmosPqXY51Qcwdc126miSnTF2bt7xgG5fyPgX",
-            "6zPRRm2tjgUYPrnSboaxr2RvovB9y5vT9f4kYyzdbbri",
-            "H9AcSjr1N9PX8pXGef98MS8sFL5p7bqLtPb71xeXo5Nn",
-            "569xM2bZSTijhdB8u15G6e442PVq3PhcJUPFUZM9njvi",
-            "BbuK8MkcZSJxx7VtRXB8Up8CqeURv3FRHzdzavPyKr1H",
-            "6SXW9UNVdiUWJhw4RvfmovbkTEBeb8tvBzPHc8EeVWi",
-            "6m8H4nKpm5fEQqKBaniRUYQfeaH7pjMkPPFiMnGXZEd8",
-            "D8W6t585rTrYfhtQQTxozyG6w2oCLpBwLCy7rMsS1rNF",
-            "7iZvgThSvJv95mkWnPPE6LENVcXTKfCVV2A4k3pZv9Te",
-            "C7QuXVtEF3dm1ocF5nsigfPqRXkdH2oNr5683gs8SacC",
-            "7SaJ5xEHTtWrFoJcNrNkJKb4pHJSDoMtzvcyHXJuhZRf",
-            "A7AwVVu1y1bzz86LQrLbVtgC43k7LES6Lesc8ao7MGFP",
-            "7rk54mPCRAg3jKCioCVK5wganTsGnn4ZYpCT9uTWzHi7",
-            "AAaXsTU7hWFUY6UPTm2MfK8esLrh2P17M2e9kHSPwRMZ",
-            "Hb2freG61xfMgmH6CUJDiyjG5zbbfcp9a6TQ2SJvs2Ur",
-            "A6VbaGsFNZ3GBiin7XMRS2cwvbVpys8AKgnRmKSNvBwX",
-            "6cNPxiBvD6xzA6UbKuNjuyRfbGpprdG3nRPdTYyZGwST",
-            "HUmPDU5amyLXjpZJGwMf6YAa6W9LW2srJjcrSM3FXgUA",
-            "5Wc3GkVnPmfRMgumstAEtT1yWYR7nBeyjYEQonz91bpb",
-            "NvbSrT458e6LCeLq257rH73dZ7btcq1TPkQVsyL8ouT",
-            "AG4harXetHAJLmKfQRTvW8uBBhjGDqbSvMQGTAhik4eE",
-            "6nqWUFzjoJzuZ9FWF6iBjFwUpGX6mZrF25WaWsbwqekM",
-            "9gGUAz4psCE2t3NFr8p64hgvXAcQGADtLmrNDTKMJPpC",
-            "45t3M59UDEkeADxyGq1tJxwoUPQ5kUxeuLsusYBFkBah",
-            "FmV9BnyyAGqVWudkV2S4hq7TGkUfoa8ku6UhKK1UN9zn",
-            "8cQaC9iDd9pGQYcZfK61zgMitBvsW7218WzG55JhonsZ",
-            "CC72FniNhUM41xSoA5h1idGxstrKXDqqhnYzfyBTSZDG",
-            "Cy1zKgjXT5PREXaw72tu8sWjxwiTpmnwn1kzhMKaCrtb",
-            "A4maCv1LM7rjpUzzM7JP4wg61VchwCovKiRqo3fbXSgF",
-            "4ervEtFsXtxBJCshqaXbHBbv7xWz4XsSp6ptDPEJRq53",
-            "93b5xNohGF24cbbGn7pw4QUKGLcrXdBVGw8gRsxTNcUx",
-            "84GSk7hjwaUwX5wxmvnHiTtSnffPYP7dnmoP6ZJ5TnSR",
-            "hLZNVV8EbeLCjkXCxfBePjAcuPSwCryWtiStoV7E4cU",
-            "HZwUnXYy27TUWQucuuSM8dYJxxCgUMgfDf83vZNz7CKe",
-            "CqVgxaQprGDZkBnAFNxKnwgaB5929CT1nSFxYjgUJz4H",
-            "Ee7EsMJ6bYN99FKVTc9SEpX9HucyHg4AGuQpRuP9Ac4c",
-            "Di5eqFCEonm8ABWfZph4fermaFbtrU5Jj2mrYdkKM1pT",
-            "bfSBHwyL6PNjpeQEyrjsnbo7TzGRttbymvN74Y9czN3",
-            "AgUfgteJ5mGWeuKZeUTedwvvu8jLaWGiRE2matEMoHEG",
-            "xQob6CJEuPD23kbMENHWGWT7ho4Anwx4dpH7pBbX6MY",
-            "6Cr9rtgY4c7H6DZzaCTgVpeFK8TpdcuhZXeZ2wqPhCD3",
-            "CssW9uGzH22u9CUkft1xgqHgKCGCTrCL6Ma93wLY94dq",
-            "GBEo8NoJAg5woWXXWXKT5xaWVUmxsqYgLkvyLkFDEgzY",
-            "4WeKjRD14BGktxENsyE6hf8SBmmxY4F56V7TEMPrQ6nR",
-            "7w3qaYPMAnbHnyWKqDzHVZcTLfJaDXhB2JdDUkZXuMho",
-            "EUdiwR5oc6YfH3Y2AzQt5fUJAPB6F1kHyiqW8ZM5m5mg",
-            "BWuHaUGmRKqGew1hffbCnfzkNNn4XLToyYp5zw8LiqSo",
-            "EGLDBGNDaC1pr3iznmGDUSVLmFE8DGR1LruUHTwCFhhy",
+            "966SEWSx1Dyx9hYMJxiUt3E2uer2HfdCgEmfBpkk5ovL",
+            "6MC1F8kUvUMRo853ZFwhQVd5mSoLxmxN1Q2s724U3Gkd",
+            "9bXJSJ3tGjkk8QVSykcGE6KnzdKznfjWP7YXicSRFTe8",
+            "JCquHzZYEnM2c4Egx31wN9BwA6ogY9Lg88Uyhfg4CTwe",
+            "9dBgnqQWLauQGZPr1R5196qgSqXdU3umVY3ZvnrExa68",
+            "9wiBzTBVo1Ls5dHbzSNbiAqJAuonA44tPGRkBz6mx5Th",
+            "AWsByD4jdTBVDJrtCzjDCE7Qcs2LNcvWCVa6BV27nMMQ",
+            "4XVJ5mZB2h5fmcMJYasc6QReQQTNhJCQpevwA666diwN",
+            "EkRnwUnggNm93G9NmPS8cGMwArffiRnmP8R6VBxbhcZX",
+            "FwiuNR91xfiUvWiBu4gieK4SFmh9qjMhYS9ebyYJ8PGj",
+            "BW3mgKMZNv9PzdwiCXLW4rzzXcNAZrs9wQc9m9Xca6rg",
+            "A5U2LbZhwc6GFZtYft6MtbGHFhYyZ1pzCRQkz7D2b2YS",
+            "2bRJLkxFbhL2aT3mjmzZegp6jQZa3UcDkuH37ePqMSuX",
+            "2WLM9BDFMpMtzRrfWtPZRrUtquwdBRnBiC8ahqS6upCS",
+            "BGz6pBLvc4nuGNsbXYoAE7z466kSjZaoXKJMnEkZWkyD",
+            "EGQ86TNvchBYmNujCdVX4LVKpqhH2MGnCYxSuREsarMn",
+            "8W7q7xurgBaNqLzc5iNTm46An1fWPCRzhPTmJyy8QYuA",
+            "Gcdv7xvxQNzrX3Ks4GF1P1TyGzJZHpe3CPr8zRJ8Q1GT",
+            "GmBMZ8BNeNR6mRFqu2ZaFvpxvGPXmc9Aa4a1GWFaxWMv",
+            "DzCbrMZXNG3XpXuufhBp233LJXvV8Xq1C3q4ksrtbCPP",
+            "29XTcKRGCtZARBSG8PZ2C41PKSeJXxFnYr7ivBMzRtse",
+            "FmuR66XKWLuXW1ZZkWwy1DThxdTtUMesBoahXu3MjNEs",
+            "4FkGNJMvKFk9PFwn8TBtk1ShUKege6D5Au87ezwLWiqk",
+            "AXSXYioiHGFvQ1XF4zCvXjnVAcMY68WQ4RH5r5nAdah5",
+            "9fY5jLin2yYMK9Eee5diWzJRZqLaYgEUupUNsKVYAMhi",
+            "5yKHz86cvHocaQnxv4mPJjftA4w7BbJzQQr5rG9miyZ3",
+            "2RKBg6QQi6MF7kjC51uFsFhDE1ydh6cNK1wY5iA8Rpdt",
+            "9PDif8wTTXbiJry8hviXtiiyCyyvLpqxWEJjPVSgEb73",
+            "9HqxfFV3DtvcALPDt3iici6naNYVa6Mzp9KV1sAwb4rX",
+            "E72s83uakUDhkx4ZHt9YAj8qJeF2wKgHr95C1T6gYJEV",
+            "2U9nDD4XNrwDU2NXXNQDDckLpBpfHdXoWoahEnu75TjB",
+            "HYCzZYupXUyamfTQWNkULW1EFcNPCTA5urnEXCFXkLRh",
+            "BRt1iVYDNoohkL1upEb8UfHE8yji6gEDAmuN9Y4yekyc",
+            "6rvpVhL9fxm2WLMefNRaLwv6aNdivZadMi56teWfSkuU",
+            "HbMbeaDH8xtB1a8WpwjNqcXBBGraKJjJ2xFkXEdAy1rY",
+            "FejEVPJBH5TEbgcpupNuRyiMRoeaCQwPvtVU6w3i2xRc",
+            "F2RieMDdnWyydUa8prEUz9Xv9k6Cny3kqF79ufTiiTTN",
+            "C3tJvXV9zrHXDSu1QXYS1WEbo2rmpZpVbDzdGALkXo4H",
+            "2dYDXu7uU5rTzrEYRYxGwBvqB6XNMHGJLyBsczYdxCX6",
+            "9PxA7PANvfiTohha8PApJ2VHCFPUHdWYnyiGdv3R6sn8",
+            "AvNeVrKZy1FaEG9suboRXNPgmnMwomiU5EvkF6jGxGrX",
+            "EWZW9aJmY2LX6ZyV5RU8waHWKF1aGaxzbuBuRQp6G4j",
+            "2SgUGxYDczrB6wUzXHPJH65pNhWkEzNMEx3km4xTYUTC",
+            "C7BLPzc1vzLL3Tm5udXELnHDRXeXkdd5f1oMKP8rwUNv",
+            "FeKSMTD9Z22kdnMLeNWAfaTXydbhVixUPcfZDW3QS1X4",
+            "5dX3tkVDmbHBWMCQMerAHTmd9wsRvmtKLoQt6qv9fHy7",
+            "7drJL7kEdJfgNzeaEYKDqTkfmiG2ain3nEPtGHDZc6i2",
+            "3vRuk97EaKACp1Z337PvVWNdab57hbDwefdi1zoUg46D",
+            "2keDsfcMY6hLvfrmzDAfnMSudMqYczPVcb8MSkK59P9r",
+            "B2Mq1fpJ2bZYxxtF4yz6nNvLJLaYzM3zQsHcs2oDqk3z",
         ];
         let pubkeys: Vec<Pubkey> = pool_addresses.iter().map(|p| { return Pubkey::from_str(*p).unwrap(); }).collect::<Vec<Pubkey>>();
 
-        rpc.get_multiple_accounts(&pubkeys[0..100]).unwrap().iter().enumerate().for_each(|(index, market_account)| {
+        rpc.get_multiple_accounts(&pubkeys).unwrap().iter().enumerate().for_each(|(index, market_account)| {
             let key: Pubkey = pubkeys.get(index).unwrap().to_owned();
             let account = market_account.to_owned().unwrap();
             let mut jupiter_invariant =
@@ -662,13 +608,6 @@ mod tests {
             let accounts_to_update = jupiter_invariant.get_accounts_to_update();
             let accounts_map = JupiterInvariant::fetch_accounts(&rpc, accounts_to_update);
             jupiter_invariant.update(&accounts_map).unwrap();
-
-            if jupiter_invariant.ticks.len() == 0 {
-                println!("fetched ticks array empty");
-            }
-            jupiter_invariant.ticks.iter().for_each(|(_, tick)| {
-                println!("{:?}", tick);
-            });
 
             let (user_transfer_authority, user_token_x_account, user_token_y_account) =
                 (Pubkey::new_unique(), Pubkey::new_unique(), Pubkey::new_unique());
@@ -692,9 +631,7 @@ mod tests {
                     quote_mint_to_referrer: None,
                     in_amount: 1, // amount doesn't matter, as the space for tickets is entirely filled.
                 };
-
-                let swap_leg_and_account_metas = jupiter_invariant.get_swap_leg_and_account_metas(&swap_params).unwrap();
-                println!("{:?}", swap_leg_and_account_metas.account_metas);
+                let _swap_leg_and_account_metas = jupiter_invariant.get_swap_leg_and_account_metas(&swap_params).unwrap();
             }
         });
     }
